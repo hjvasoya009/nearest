318d4e95d540193217754c5a64b74f473527caad
Hardikkumar Vasoya <hardikkumarvasoya@MacBook-Pro.local>
2020-07-22 22:58:14 -0700

    Theme name changed

--- functions.php
+++ functions.php
@@ -1,9 +1,9 @@
 <?php
 
 // Change all instances of THEMENAME to your theme name.
-if (!function_exists('sixpointone_setup')) :
+if (!function_exists('nearest_setup')) :
 
-    function sixpointone_setup()
+    function nearest_setup()
     {
         add_theme_support('automatic-feed-links');
         add_theme_support('title-tag');
@@ -24,17 +24,17 @@ if (!function_exists('sixpointone_setup')) :
 
 endif;
 
-add_action('after_setup_theme', 'sixpointone_setup');
+add_action('after_setup_theme', 'nearest_setup');
 
-function sixpointone_scripts_styles()
+function nearest_scripts_styles()
 {
-    wp_enqueue_style('sixpointone_style', get_stylesheet_uri());
+    wp_enqueue_style('nearest_style', get_stylesheet_uri());
     
     // Google Font
-    wp_enqueue_style('sixpointone_google_fonts', "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap");
+    wp_enqueue_style('nearest_google_fonts', "https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,400;1,600;1,700;1,800&display=swap");
 
     // Font Awesome
-    wp_enqueue_style('sixpointone_font_awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css');
+    wp_enqueue_style('nearest_font_awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-2/css/all.min.css');
 }
 
-add_action('wp_enqueue_scripts', 'sixpointone_scripts_styles');
+add_action('wp_enqueue_scripts', 'nearest_scripts_styles');
