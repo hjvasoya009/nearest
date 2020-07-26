1abe6cc868819d59057f12df14f307638dfbc8a5
Ken Tauchi <kentauchica@gmail.com>
2020-07-26 01:02:52 -0700

    style the team page

--- template-parts/content-team-member.php
+++ template-parts/content-team-member.php
@@ -1,18 +1,21 @@
 <?php ?>
+
 <article <?php post_class(); ?>>
 
     <div class="post-thumbnail">
         <?php the_post_thumbnail(); ?>
     </div>
 
-    <h2><?php the_title(); ?></h2>
+    <div class="team-meta-data">
+        <h2><?php the_title(); ?></h2>
 
-    <div class="team-member-postion">
-        <?php the_field('team_member_position'); ?>
-    </div>
+        <div class="team-member-postion">
+            <?php the_field('team_member_position'); ?>
+        </div>
 
-    <div class="team-member-description">
-        <?php the_field('team_member_description'); ?>
+        <div class="team-member-description">
+            <?php the_field('team_member_description'); ?>
+        </div>
     </div>
 
 </article>
