337b28b9c195f67bd7446179de59d0f5e185bae3
Hardikkumar Vasoya <hardikkumarvasoya@MacBook-Pro.local>
2020-07-27 10:55:36 -0700

    JS added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// console.log('Connected!');
+var rightOutlineHTML = `<div class="curl curl-right">
+<div class="strand strand1"></div>
+<div class="strand strand2"></div>
+<div class="strand strand3"></div>
+<div class="strand strand4"></div>
+<div class="strand strand5"></div>
+<div class="strand strand6"></div>
+<div class="strand strand7"></div>
+<div class="strand strand8"></div>
+<div class="strand strand9"></div>
+<div class="strand strand10"></div>
+<div class="strand strand11"></div>
+<div class="strand strand12"></div>
+<div class="strand strand13"></div>
+<div class="strand strand14"></div>
+<div class="strand strand15"></div>
+</div>`;
+
+$(".about-section").before(rightOutlineHTML);
